2197e5401cc821333ca500c28cb30015984ce673
Derek <derek-wind@hotmail.com>
2023-08-08 14:31:27 +0800

    no message

--- bloodandmind/ViewController.swift
+++ bloodandmind/ViewController.swift
@@ -8,12 +8,47 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        configurCalendarView()
         // Do any additional setup after loading the view.
     }
-
-
+    private func configurCalendarView(){
+        let calendarView = UICalendarView()
+        calendarView.calendar = .current
+        calendarView.locale = .current
+        calendarView.fontDesign = .rounded
+        calendarView.layer.cornerRadius = 15
+        
+        let dateSelection = UICalendarSelectionSingleDate(delegate:self)
+        calendarView.selectionBehavior = dateSelection
+        calendarView.delegate = self
+        
+        calendarView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(calendarView)
+        NSLayoutConstraint.activate([
+            calendarView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            calendarView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            calendarView.topAnchor.constraint(equalTo: view.topAnchor,constant: 100),
+            calendarView.heightAnchor.constraint(equalToConstant: 300)
+        
+        
+        
+        ])
+    }
 }
 
+extension ViewController:UICalendarViewDelegate,UICalendarSelectionSingleDateDelegate{
+    func dateSelection(_ selection: UICalendarSelectionSingleDate, didSelectDate dateComponents: DateComponents?) {
+        
+    }
+    
+    func calendarView(_ calendarView: UICalendarView, decorationFor dateComponents: DateComponents) -> UICalendarView.Decoration? {
+        return .customView{
+            let smileFace = UILabel()
+            smileFace.text = "😃"
+            return smileFace
+        }
+    }
+}
